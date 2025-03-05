cd94c6e2163e9fcb906f64395475ebd4cc855934
Bartlomiej <bartek21122006@gmail.com>
2025-03-05 18:39:57 +0100

    continuation of implementing the RecursionBasics module;

--- src/main/java/DsaFundamentals/RecursionBasics/RecursionBasics.java
+++ src/main/java/DsaFundamentals/RecursionBasics/RecursionBasics.java
@@ -13,6 +13,12 @@ public class RecursionBasics {
         System.out.println();
 
         System.out.println(findFibonacciNth(6));
+
+        System.out.println(pow(2, 4));
+
+        System.out.println(fastPow(2, 31));
+
+        System.out.println(isPowerOfThree(28));
     }
 
     private static int factorial(int n) {
@@ -33,7 +39,26 @@ public class RecursionBasics {
     }
 
     private static int findFibonacciNth(int n) {
-        if (n <= 2) return 1;
+        if (n <= 0) throw new IllegalArgumentException();
+        else if (n <= 2) return 1;
         return findFibonacciNth(n - 1) + findFibonacciNth(n - 2);
+    }
+
+    private static int pow(int base, int exponent) {
+        if (exponent == 1) return base;
+        return base * pow(base, exponent - 1);
+    }
+
+    private static int fastPow(int base, int exponent) {
+        if (exponent == 1) return base;
+        int half = fastPow(base, exponent / 2);
+        if (exponent % 2 == 0) return half * half;
+        else return base * half * half;
+    }
+
+    private static boolean isPowerOfThree(int n) {
+        if (n == 1) return true;
+        else if (n % 3 != 0) return false;
+        return isPowerOfThree(n / 3);
     }
 }
