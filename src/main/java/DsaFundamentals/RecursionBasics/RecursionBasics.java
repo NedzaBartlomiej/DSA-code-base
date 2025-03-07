2ff74f560838e8b628da32f9a69f0e040bec5b98
Bartlomiej <bartek21122006@gmail.com>
2025-03-07 21:36:52 +0100

    continuation of the RecursionBasics module implementation;

--- src/main/java/DsaFundamentals/RecursionBasics/RecursionBasics.java
+++ src/main/java/DsaFundamentals/RecursionBasics/RecursionBasics.java
@@ -19,6 +19,19 @@ public class RecursionBasics {
         System.out.println(fastPow(2, 31));
 
         System.out.println(isPowerOfThree(28));
+
+        System.out.println(calculateTheSumOfNumbersFrom1ToNUsingRecursion(3));
+
+        printTheFibonacciSeriesUpToNTermsUsingRecursion(0, 1, 5);
+
+        System.out.println();
+        int[] arr = {1, 2, 3, 4, 5, 6, 6345};
+        printTheElementsOfAnArrayUsingRecursion(arr, arr.length - 1);
+
+        System.out.println();
+        System.out.println(countTheDigitsOfAGivenNumberUsingRecursion(13434, 0));
+
+        System.out.println(findTheSumOfDigitsOfANumberUsingRecursion(345));
     }
 
     private static int factorial(int n) {
@@ -60,5 +73,35 @@ public class RecursionBasics {
         if (n == 1) return true;
         else if (n % 3 != 0) return false;
         return isPowerOfThree(n / 3);
+    }
+
+    private static int calculateTheSumOfNumbersFrom1ToNUsingRecursion(int n) {
+        if (n == 1) return 1;
+        return n + calculateTheSumOfNumbersFrom1ToNUsingRecursion(n - 1);
+    }
+
+    private static void printTheFibonacciSeriesUpToNTermsUsingRecursion(int first, int sec, int n) {
+        if (first + sec > n) return;
+        System.out.print(first + sec + ";");
+        printTheFibonacciSeriesUpToNTermsUsingRecursion(sec, first + sec, n);
+    }
+
+    private static void printTheElementsOfAnArrayUsingRecursion(int[] arr, int idx) {
+        if (idx < 0) return;
+        printTheElementsOfAnArrayUsingRecursion(arr, idx - 1);
+        System.out.print(arr[idx] + ";");
+    }
+
+    // but I think the lvl approach is redundant, better way would be simply documentation,
+    // which informs the client about case 0, and can be handled by the client.
+    private static int countTheDigitsOfAGivenNumberUsingRecursion(int n, int lvl) {
+        if (lvl == 0 && n == 0) return 1;
+        if (n == 0) return 0;
+        return 1 + countTheDigitsOfAGivenNumberUsingRecursion(n / 10, ++lvl);
+    }
+
+    private static int findTheSumOfDigitsOfANumberUsingRecursion(int n) {
+        if (n == 0) return 0;
+        return n % 10 + findTheSumOfDigitsOfANumberUsingRecursion(n / 10);
     }
 }
