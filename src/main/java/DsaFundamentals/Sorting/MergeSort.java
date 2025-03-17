b028a827edc8f7bb12a6c6a235df90ea83307eed
Bartlomiej <bartek21122006@gmail.com>
2025-03-17 20:44:11 +0100

    Module: 'Sorting' -> Topic: 'MergeSort' -> Added 'Count Inversions' solution;

--- src/main/java/DsaFundamentals/Sorting/MergeSort.java
+++ src/main/java/DsaFundamentals/Sorting/MergeSort.java
@@ -6,10 +6,10 @@ public class MergeSort {
     private static int[] merged;
 
     public static void main(String[] args) {
-        int[] arr = {3, 5, 435, 3, 1, 234, 45, 788, 7, 4, 324, 2, 9, 10};
+        int[] arr = {4, 3, 2, 1};
         merged = new int[arr.length];
         System.out.println(Arrays.toString(arr));
-        iterativeMergeSort(arr);
+        System.out.println("Inversion count = " + iterativeMergeSort(arr));
         System.out.println(Arrays.toString(arr));
     }
 
@@ -20,8 +20,11 @@ public class MergeSort {
         mergeSort(arr, l, mid);
         mergeSort(arr, mid + 1, r);
         // merge
+        // grabbing the partly sorted main array
         System.arraycopy(arr, 0, merged, 0, arr.length);
         int i = l, j = mid + 1, curr = l;
+
+        // partly-sorting on the main array basing on mergeArr
         while (i <= mid && j <= r) {
             if (merged[i] <= merged[j]) {
                 arr[curr] = merged[i++];
@@ -30,41 +33,50 @@ public class MergeSort {
             }
             curr++;
         }
-
         while (i <= mid) arr[curr++] = merged[i++];
     }
 
-    private static void iterativeMergeSort(int[] arr) {
+    private static int iterativeMergeSort(int[] arr) {
+        int inversionCounter = 0;
         for (int mergeSize = 1; mergeSize < arr.length; mergeSize *= 2) {
             // sub-arrays merging
             for (int l = 0; l < arr.length - 1; l += 2 * mergeSize) {
                 int mid = l + mergeSize - 1;
                 int r = Math.min(l + (mergeSize * 2) - 1, arr.length - 1);
-                merge(arr, l, mid, r);
+                inversionCounter += merge(arr, l, mid, r);
             }
         }
+        return inversionCounter;
     }
 
-    private static void merge(int[] arr, int l, int mid, int r) {
-        int i = l;
-        int j = mid + 1;
+    private static int merge(int[] arr, int l, int mid, int r) {
+        int leftIterator = l;
+        int rightIterator = mid + 1;
         int[] mergeArr = new int[r - l + 1];
-        int iterator = 0;
+        int mergeIterator = 0;
+        int inversionCounter = 0;
 
         // merging
-        while (i <= mid && j <= r) {
-            if (arr[i] <= arr[j]) {
-                mergeArr[iterator] = arr[i++];
+        while (leftIterator <= mid && rightIterator <= r) {
+            if (arr[leftIterator] <= arr[rightIterator]) {
+                mergeArr[mergeIterator] = arr[leftIterator++];
             } else {
-                mergeArr[iterator] = arr[j++];
+                inversionCounter += mid - leftIterator + 1;
+                mergeArr[mergeIterator] = arr[rightIterator++];
             }
-            iterator++;
+            mergeIterator++;
         }
         // remaining elms
-        while (i <= mid) mergeArr[iterator++] = arr[i++];
-        while (j <= r) mergeArr[iterator++] = arr[j++];
+        while (leftIterator <= mid) {
+            mergeArr[mergeIterator++] = arr[leftIterator++];
+        }
+        while (rightIterator <= r) {
+            mergeArr[mergeIterator++] = arr[rightIterator++];
+        }
 
         // updating the main array
         System.arraycopy(mergeArr, 0, arr, l, mergeArr.length);
+
+        return inversionCounter;
     }
 }
